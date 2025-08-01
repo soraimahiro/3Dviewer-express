9dbb6c0dc83dd9c106c134caf8ecf96c77ec9030
soraimahiro <cherngci96@gmail.com>
2025-08-01 14:35:40 +0800

    add buttom in toolbar

--- public/js/params.js
+++
@@ -1,6 +0,0 @@
-export default function getParams(){
-    const params = new URLSearchParams(window.location.search);
-    const path = params.get('path');
-    console.log('Path:', path);
-    return path;
-}
