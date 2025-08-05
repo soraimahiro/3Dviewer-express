3211c3b5c85f3c8279d99a43c66d8981ddaccd1d
soraimahiro <cherngci96@gmail.com>
2025-08-05 10:58:31 +0800

    完善toolbar項目及功能

--- app.js
+++ app.js
@@ -3,13 +3,32 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fs = require('fs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(express.json({limit: '50mb'}));
+// Clear screenshots folder on startup
+function clearScreenshotsFolder() {
+    const screenshotsDir = path.join(__dirname, 'public/screenshots');
+    if (fs.existsSync(screenshotsDir)) {
+        const files = fs.readdirSync(screenshotsDir);
+        files.forEach(file => {
+            const filePath = path.join(screenshotsDir, file);
+            if (fs.statSync(filePath).isFile()) {
+                fs.unlinkSync(filePath);
+            }
+        });
+        console.log('Screenshots folder cleared');
+    }
+}
+
+// Clear screenshots on startup
+clearScreenshotsFolder();
+
+app.use(express.json({ limit: '50mb' }));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,19 +44,19 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
